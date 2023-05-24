15af4782ec20b453f98594ee3dc2e113fefeb0b5
sergioHEFE <121230365+SergioHenriqueFe@users.noreply.github.com>
2023-05-23 21:07:33 -0300

    retirando a faixa de debug

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Início',
       theme: ThemeData(primarySwatch: Colors.green),
       initialRoute: '/splash',
